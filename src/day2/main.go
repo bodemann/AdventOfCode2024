25a5da82a60a2448130fd545f713bd1f405d0c97
Jörn Bodemann <jb@opv.de>
2024-12-03 11:51:18 +0100

    Days 2 and 3 added

--- src/day2/main.go
+++ src/day2/main.go
@@ -1,5 +1,69 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"slices"
+	"sort"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	rawData, err := os.ReadFile("/Users/jb/tmp/GoTmp/aoc2024day2/src/data.txt")
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(string(rawData), "\n")
+	var safePartOne int
+	var safePartTwo int
+	var safeWithDampener int
+	for _, line := range lines {
+		data := convertStringOfNumbersToIntegers(line, " ")
+		// Make all "reports" (lines in the file) increasing
+		if data[0] > data[1] {
+			slices.Reverse(data)
+		}
+		var differences []int
+		for idx := 0; idx < len(data)-1; idx++ {
+			differences = append(differences, data[idx+1]-data[idx])
+		}
+		// Sorting the differences makes it easy to check for to big or small differences, since we only have to find one error.
+		sort.Ints(differences)
+		// This is the code for part 1
+		if differences[0] >= 1 && differences[len(differences)-1] < 4 {
+			safePartOne++
+		}
+
+		// This is the code for part 2
+		var countErrors int
+		for _, difference := range differences {
+			if difference < 1 || difference > 3 {
+				countErrors++
+			}
+		}
+		switch countErrors {
+		case 0:
+			safePartTwo++
+		case 1:
+			safeWithDampener++
+		case 2:
+			fmt.Println(differences)
+		}
+		//fmt.Println(differences, countErrors, safePartTwo, safeWithDampener)
+	}
+	fmt.Println("Safe Part One:", safePartOne, "   Safe Part Two: ", safePartTwo, "   SafeWithDampener: ", safePartTwo+safeWithDampener)
+}
+
+func convertStringOfNumbersToIntegers(line string, sep string) []int {
+	data := strings.Split(line, sep)
+	var result []int
+	for _, str := range data {
+		number, err := strconv.Atoi(str)
+		if err != nil {
+			panic(err)
+		}
+		result = append(result, number)
+	}
+	return result
 }
---
+++ src/day3/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	rawData, err := os.ReadFile("/Users/jb/tmp/GoTmp/aoc2024day3/src/data.txt")
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(string(rawData), "\n")
+
+	rMul, _ := regexp.Compile(`mul\(\d+,\d+\)`)
+	rNum, _ := regexp.Compile(`\d+`)
+	var sum int
+	for _, line := range lines {
+		mulsStr := (rMul.FindAllString(line, -1))
+		for _, mul := range mulsStr {
+			numStrs := (rNum.FindAllString(mul, -1))
+			numInts := convertStringsToIntegers(numStrs)
+			sum += numInts[0] * numInts[1]
+		}
+	}
+	fmt.Println(sum)
+}
+
+func convertStringsToIntegers(strs []string) []int {
+	var result []int
+	for _, str := range strs {
+		number, err := strconv.Atoi(str)
+		if err != nil {
+			panic(err)
+		}
+		result = append(result, number)
+	}
+	return result
+}
